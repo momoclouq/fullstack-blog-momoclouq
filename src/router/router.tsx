dd3dd3dea21a2458e881d82cf86713386a927bd8
momoclouq <phamhoangminh050899@gmail.com>
2022-10-07 09:10:40 +0700

    Add first post

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
 import HomePage from '../pages/HomePage.mdx';
 import ErrorPage from '../pages/ErrorPage.mdx';
 import AboutMePage from '../pages/AboutMePage.mdx';
 import ProjectPage from '../pages/ProjectPage.mdx';
 import ContactMePage from '../pages/ContactMePage.mdx';
-import App from "../App";
+
+import BlogPageCreation from '../posts/BlogPageCreation/BlogPageCreation.mdx';
 
 const router = createBrowserRouter([
     {
@@ -28,6 +31,10 @@ const router = createBrowserRouter([
             {
                 path: "/contact-me",
                 element: <ContactMePage />
+            },
+            {
+                path: "/posts/personal-page-creation",
+                element: <BlogPageCreation />,
             }
         ]
     }
